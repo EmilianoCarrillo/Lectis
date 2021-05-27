523c72ec8b6c7e2dc759900a9013d26e395468c0
Emiliano Carrillo <emiliano.carrillo.moncayo@gmail.com>
2021-05-27 15:43:05 -0500

    fix Link

--- components/boton-categoria.js
+++ components/boton-categoria.js
@@ -1,13 +1,13 @@
 import styles from './boton-categoria.module.scss'
 import utils from '../styles/utils.module.css'
-import Link from 'next/Link'
+import NextLink from 'next/Link'
 import Image from 'next/image'
 
 export default function BotonCategoria({ categoria }) {
   const { nombre, icono, slug } = categoria.fields
 
   return (
-    <Link href={'/categorias/' + slug}>
+    <NextLink href={'/categorias/' + slug}>
       <div className={styles.container} >
         {/* <Image src={'https:' + icono.fields.file.url} 
           width={icono.fields.file.details.image.width}
@@ -16,6 +16,6 @@ export default function BotonCategoria({ categoria }) {
         <img src={'https:' + icono.fields.file.url}/>
         <h3 className={utils.subtitle_regular}>{ nombre}</h3>
       </div>
-    </Link>
+    </NextLink>
   )
 }
