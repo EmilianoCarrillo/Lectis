d997415adb83d51483b887ae35cacca79ef913da
Emiliano Carrillo <emiliano.carrillo.moncayo@gmail.com>
2021-07-13 14:47:21 -0500

    variable height tab bar and active fixed not on path

--- components/nav-bar-btn.js
+++ components/nav-bar-btn.js
@@ -3,12 +3,16 @@ import utils from '../styles/utils.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
 
-export default function NavBarButton({label, isActive, href}) {
+export default function NavBarButton({label, isActive, href, onClick}) {
   return (
     <Link href={href}>
-    <li className={styles.wrapper}>
+    <li className={styles.wrapper} onClick={onClick}>
       <div className={styles.image}>
-        <Image src={`/assets/${label}${isActive ? '-active' : ''}.svg`} width={24} height={24}/>
+        { isActive ? 
+        <Image src={`/assets/${label}-active.svg`} width={24} height={24} priority/>
+        :
+        <Image src={`/assets/${label}.svg`} width={24} height={24} priority/>
+        }
       </div>
       <p className={`${utils.small_regular} ${styles.label} ${isActive && styles.active}`}>{label}</p>
     </li>
--- components/nav-bar.js
+++ components/nav-bar.js
@@ -1,14 +1,65 @@
-import NavBarButton from './nav-bar-btn'
-import styles from './nav-bar.module.scss'
+import NavBarButton from "./nav-bar-btn";
+import styles from "./nav-bar.module.scss";
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
 export default function NavBar() {
   const path = useRouter().pathname;
+  const [activeButton, setActiveButton] = useState("Inicio");
+  const [scrollDir, setScrollDir] = useState("scrolling down");
+
+  useEffect(() => {
+    const threshold = 5;
+    let lastScrollY = window.pageYOffset;
+    let ticking = false;
+
+    const updateScrollDir = () => {
+      const scrollY = window.pageYOffset;
+
+      if (Math.abs(scrollY - lastScrollY) < threshold) {
+        ticking = false;
+        return;
+      }
+      setScrollDir(scrollY > lastScrollY ? "scrolling down" : "scrolling up");
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+      ticking = false;
+    };
+
+    const onScroll = () => {
+      if (!ticking) {
+        window.requestAnimationFrame(updateScrollDir);
+        ticking = true;
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [scrollDir]);
+
   return (
-    <ul className={styles.wrapper}>
-      <NavBarButton label='Inicio' href='/' isActive={path == '/'}/>
-      <NavBarButton label='Perfil' href='/Perfil' isActive={path == '/Perfil'}/>
-      <NavBarButton label='Buscar' href='/Buscar' isActive={path == '/Buscar'}/>
+    <ul
+      className={`${styles.wrapper} ${
+        scrollDir == "scrolling down" && styles.scrollingDown
+      }`}
+    >
+      <NavBarButton
+        label="Inicio"
+        href="/"
+        isActive={activeButton == 'Inicio'}
+        onClick={() => setActiveButton('Inicio')}
+      />
+      <NavBarButton
+        label="Perfil"
+        href="/Perfil"
+        isActive={activeButton == 'Perfil'}
+        onClick={() => setActiveButton('Perfil')}
+      />
+      <NavBarButton
+        label="Buscar"
+        href="/Buscar"
+        isActive={activeButton == 'Buscar'}
+        onClick={() => {setActiveButton('Buscar')}}
+      />
     </ul>
-  )
+  );
 }
